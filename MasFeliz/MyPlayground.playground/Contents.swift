a067751bf7e39472cebbd4e3250a7675485b5391
hansellesteban <hansell.ramos@gmail.com>
2016-05-14 12:17:19 -0500

    First Commit

---
+++ MasFeliz/MyPlayground.playground/Contents.swift
@@ -0,0 +1,21 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+//variables y constantes
+var nombre = "Hansel"
+let edad = 26
+
+//arreglos
+var deportes = ["Futbol"]
+deportes.append("Natación")
+deportes.append("Golf")
+
+deportes[2]
+
+//opcionales
+var apellido:String? = "Ramos"
+apellido = nil
+
--- MasFeliz/ViewController.swift
+++ MasFeliz/ViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let colores = Colores()
+    let frases = Datos()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +23,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func dameUnMensajePositivo() {
+        mensajePositivo.text = frases.regresarFraseFeliz()
+        let colorAleatorio = colores.regresarColorAleatorio()
+        view.backgroundColor = colorAleatorio
+        
+    }
 
+    @IBOutlet weak var mensajePositivo: UILabel!
 }
 
